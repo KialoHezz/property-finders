114c2b7f04e7224fa4359ba35e89968625bc9d2c
KialoHezz <hezzykialo.official.ke@gmail.com>
2023-05-17 14:56:26 +0300

    Create new controller[:dashboard]

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'dashboard/index'
+  get 'dashboard/properties'
+  get 'dashboard/reports'
   resources :properties
   devise_for :accounts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
