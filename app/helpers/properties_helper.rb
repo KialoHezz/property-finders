eddc67fd981d06010dec64b3e74fb79e47c230f8
KialoHezz <hezzykialo.official.ke@gmail.com>
2023-05-17 17:54:18 +0300

    scope latest/mock-up[:lates]

--- app/helpers/properties_helper.rb
+++ app/helpers/properties_helper.rb
@@ -1,2 +1,8 @@
 module PropertiesHelper
+      # Individual property doesn't have img then this helper to set a default image
+      def property_thumbnail property
+        img = property.photo.present? ? property.photo.thumb.url : "placeholder.png"
+        # return a image tag
+        image_tag img, class: "property-thumb"
+    end
 end
--- app/models/property.rb
+++ app/models/property.rb
@@ -2,4 +2,7 @@ class Property < ApplicationRecord
     mount_uploader :photo, PhotoUploader
 
     belongs_to :account
+
+    # set a new scope
+    scope :latest, -> { order created_at: :desc }
 end
