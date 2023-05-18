3f78debc6c4533bfc8767a7470d5344c88919bda
KialoHezz <hezzykialo.official.ke@gmail.com>
2023-05-18 14:31:45 +0300

    Style show

--- README.md
+++ README.md
@@ -22,3 +22,6 @@ Things you may want to cover:
 * Deployment instructions
 
 * ...
+
+
+* yarn add @fortawesome/fontawesome-free
--- app/helpers/properties_helper.rb
+++ app/helpers/properties_helper.rb
@@ -1,8 +1,14 @@
 module PropertiesHelper
+
       # Individual property doesn't have img then this helper to set a default image
       def property_thumbnail property
         img = property.photo.present? ? property.photo.thumb.url : "placeholder.png"
         # return a image tag
+        image_tag img, class: "property-thumb"
+    end
+
+    def property_thumbnail_url property
+        img = property.photo.present? ? property.photo.thumb.url : "placeholder.png"
         image_tag img, class: "property-thumb"
     end
 
