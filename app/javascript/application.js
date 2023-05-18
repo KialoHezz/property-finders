8b88d85b1a1a9e7a26e106c0efa6f9d0ca55ef6b
KialoHezz <hezzykialo.official.ke@gmail.com>
2023-05-18 10:25:59 +0300

    Show More Btn functionality

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+
+const details= document.getElementById("details");
+
+details.addEventListener("click", () =>{
+    const propDetails = document.getElementById("prop-details");
+    
+    console.log(propDetails.classList.toggle("open"));
+});
