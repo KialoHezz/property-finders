1e820dd518f6246362209945859f4fe91da0353c
KialoHezz <hezzykialo.official.ke@gmail.com>
2023-05-18 15:58:41 +0300

    User profile

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,6 +4,11 @@ class DashboardController < ApplicationController
   def index
   end
 
+  def profile
+    @account = Account.find(params[:id])
+
+  end
+
   private
   def set_sidebar
     @show_sidebar = true
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
-  get "dashboard" => 'dashboard#index', as: :dashboard
-
-  get 'dashboard/properties'
-  get 'dashboard/reports'
+  
+  devise_for :accounts
   resources :properties
+  
+  get "dashboard" => 'dashboard#index', as: :dashboard
+  get "/profile/:id" => 'dashboard#profile', as: :profile
 
   # devise scope
   devise_scope :account do
     get 'accounts/sign_out' => "devise/sessions#destroy"
   end
 
-  devise_for :accounts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
   # Defines the root path route ("/")
