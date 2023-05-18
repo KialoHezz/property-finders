ec8ab3a705d6606211e0ae0bac5a634cd1f35d30
KialoHezz <hezzykialo.official.ke@gmail.com>
2023-05-18 15:49:43 +0300

    agent styling

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,5 +1,9 @@
 class PublicController < ApplicationController
   def main
+    # redirect user once signed in
+    if account_signed_in?
+      redirect_to dashboard_path and return
+    end
     @properties = Property.latest
   end
 end
