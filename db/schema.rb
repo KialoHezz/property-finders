8511cd818c853465ebe4542d3cf13f556cc04f26
KialoHezz <hezzykialo.official.ke@gmail.com>
2023-05-18 17:13:48 +0300

    Decide whether property is for rent or for sale

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_18_074749) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_18_130803) do
   create_table "accounts", force: :cascade do |t|
     t.string "first_name", default: "", null: false
     t.string "last_name", default: "", null: false
@@ -49,6 +49,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_18_074749) do
     t.datetime "updated_at", null: false
     t.integer "parking_spaces", default: 0
     t.text "details"
+    t.boolean "for_sale", default: true
     t.index ["account_id"], name: "index_properties_on_account_id"
   end
 
