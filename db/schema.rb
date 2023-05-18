24fc055819df6aa0fbf2d5ca387273d3871ca6cc
KialoHezz <hezzykialo.official.ke@gmail.com>
2023-05-18 10:57:21 +0300

    add column for company into application

--- db/schema.rb
+++ db/schema.rb
@@ -32,6 +32,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_18_074749) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image"
+    t.string "company"
     t.index ["email"], name: "index_accounts_on_email", unique: true
     t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
   end
