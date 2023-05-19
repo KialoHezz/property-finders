5b2d82173cf96f55ebb4b9694b94a631182ea6ea
KialoHezz <hezzykialo.official.ke@gmail.com>
2023-05-19 22:03:55 +0300

    Mutiple users[:admin, :normaluser]

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,13 +1,15 @@
 class AdminController < ApplicationController
-    # check is admin logged in
     before_action :can_access?
+    # check is admin logged in
     def accounts
         @accounts =Account.where(admin: false)
     end
 
     def can_access?
+      @show_sidebar = true
+
         unless current_account.admin?
-            redirect_to root_url
+            redirect_to root_url and return
         end
     end
 end
--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -2,7 +2,9 @@ class PublicController < ApplicationController
   def main
     # redirect user once signed in
     if account_signed_in?
-      redirect_to dashboard_path and return
+      path = current_account.admin? ? accounts_path: dashboard_path
+
+      redirect_to path and return
     end
     @properties = Property.latest
   end
