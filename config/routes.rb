ca3a0760b2ee9fe409b299396f41b84e9d0fa986
KialoHezz <hezzykialo.official.ke@gmail.com>
2023-05-22 16:44:05 +0300

    Scaffold for Post

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,22 @@
 Rails.application.routes.draw do
-  
-  devise_for :accounts
-  resources :properties
+  resources :posts
   # admin route
   get "/accounts" => 'admin#accounts', as: :accounts
 
+
+  
   get "/dashboard" => 'dashboard#index', as: :dashboard
   get "/profile/:id" => 'dashboard#profile', as: :profile
+  get 'dashboard/properties'
+  get 'dashboard/reports'
+  resources :properties
 
+  
   # devise scope
   devise_scope :account do
     get 'accounts/sign_out' => "devise/sessions#destroy"
   end
+  devise_for :accounts
 
   post "/agent/message" => "properties#email_agent", as: :email_agent
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
