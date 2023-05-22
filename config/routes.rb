dfddbe1b60882be22214c72233c112b7ca70ac11
KialoHezz <hezzykialo.official.ke@gmail.com>
2023-05-22 22:27:30 +0300

    Scope for blog , template styling[blog]

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,13 @@ Rails.application.routes.draw do
   
   resources :posts
   resources :properties
+
+  get "/blog" => "posts#latest", as: :blog
   
   # admin routes
   get "/accounts" => 'admin#accounts', as: :accounts
   
-  get "dashboard" => 'dashboard#index', as: :dashboard
+  get "/dashboard" => 'dashboard#index', as: :dashboard
   get "/profile/:id" => 'dashboard#profile', as: :profile
   post "/agent/message" => "properties#email_agent", as: :email_agent
   
